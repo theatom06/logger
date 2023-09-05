aea27fa2abb289a688426f0bceba034a523569d9
Carbon <119428619+atom06@users.noreply.github.com>
2023-09-05 20:15:45 +0530

    1.0.1: I pushed test code and dist, sorry im dum

--- dist/logs.d.ts
+++
@@ -1,7 +0,0 @@
-export default function createLoggerModule(projectName: string): Promise<{
-    warn: (message: string, timestamp?: number) => Promise<void>;
-    error: (message: string, timestamp?: number) => Promise<void>;
-    info: (message: string, timestamp?: number) => Promise<void>;
-    log: (message: string, timestamp?: number) => Promise<void>;
-}>;
-//# sourceMappingURL=logs.d.ts.map
